9e304f5cf2ff87d31cab8ab7b2462aad51efa76e
Jose Alvis <jalvis1491@gmail.com>
2020-08-26 09:56:19 -0400

    Adds repositories

---
+++ CRUDTimeOffRequests/Repository/Repositories/TimeOffRequestRepository.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Entities;
+using Repository.Contrats;
+using Repository.UoW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository.Repositories
+{
+    public class TimeOffRequestRepository : IRepository<TimeOffRequestsEntity>
+    {
+
+
+        private UnitOfWork unitOfWork;
+
+        public UnitOfWork UnitOfWork { get => unitOfWork; set => unitOfWork = value; }
+
+        public TimeOffRequestRepository(UnitOfWork uow)
+        {
+            if (uow == null) throw new ArgumentNullException("The unit of work is not provided");
+            unitOfWork = uow;
+        }
+        public List<TimeOffRequestsEntity> GetAll()
+        {
+            return unitOfWork.Repository<TimeOffRequestsEntity>().Search(cd => cd.Active == true).ToList();
+        }
+
+        public TimeOffRequestsEntity GetById(int modelId)
+        {
+            return unitOfWork.Repository<TimeOffRequestsEntity>().Find(cd => cd.TimeOffRequestId == modelId);
+        }
+
+        public void Remove(int modelId)
+        {
+            var entity = GetById(modelId);
+            entity.Active = false;
+            unitOfWork.Repository<TimeOffRequestsEntity>().Update(entity);
+            this.unitOfWork.Commit();
+        }
+
+        public void Save(TimeOffRequestsEntity model)
+        {
+            unitOfWork.Repository<TimeOffRequestsEntity>().Add(model);
+            unitOfWork.Commit();
+        }
+    }
+}
---
+++ CRUDTimeOffRequests/Repository/Repositories/TimeOffTypeRepository.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using Repository.Contrats;
+using Repository.UoW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository.Repositories
+{
+    public class TimeOffTypeRepository : IRepository<TimeOffTypesEntity>
+    {
+
+        private UnitOfWork unitOfWork;
+
+        public UnitOfWork UnitOfWork { get => unitOfWork; set => unitOfWork = value; }
+
+        public TimeOffTypeRepository(UnitOfWork uow)
+        {
+            if (uow == null) throw new ArgumentNullException("The unit of work is not provided");
+            unitOfWork = uow;
+        }
+        public List<TimeOffTypesEntity> GetAll()
+        {
+            return unitOfWork.Repository<TimeOffTypesEntity>().Search(cd => cd.Active == true).ToList();
+        }
+
+        public TimeOffTypesEntity GetById(int modelId)
+        {
+            return unitOfWork.Repository<TimeOffTypesEntity>().Find(cd => cd.TimeOffTypeId == modelId);
+        }
+
+        public void Remove(int modelId)
+        {
+            var entity = GetById(modelId);
+            entity.Active = false;
+            unitOfWork.Repository<TimeOffTypesEntity>().Update(entity);
+            this.unitOfWork.Commit();
+        }
+
+        public void Save(TimeOffTypesEntity model)
+        {
+            unitOfWork.Repository<TimeOffTypesEntity>().Add(model);
+            unitOfWork.Commit();
+        }
+    }
+}
